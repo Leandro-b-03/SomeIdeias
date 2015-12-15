b6e5822af207842ac593ea1f6b1bc8c258b6b0dd
Leandro <leandro.b.03@gmail.com>
2015-12-15 11:04:27 -0200

    Home

--- config/app.php
+++ config/app.php
@@ -1,4 +1,7 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
+setlocale(LC_TIME, 'ptb', 'pt_BR', 'portuguese-brazil', 'bra', 'brazil', 'pt_BR.utf-8', 'pt_BR.iso-8859-1', 'br');
 
 return [
 
@@ -39,7 +42,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +55,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt_BR',
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +68,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => 'pt_BR',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +81,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'PuAf9vxcQ2HTJzCXxuPtPRQXSRaodrbk'),
 
     'cipher' => 'AES-256-CBC',
 
@@ -95,7 +98,22 @@ return [
     |
     */
 
-    'log' => 'single',
+    'log' => 'daily',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Version Number of the App
+    |--------------------------------------------------------------------------
+    |
+    | Here come the version of the app for loging and features of the project.
+    | Get tue concept from github changelog of all commit and push to the
+    | server and bring some kind of documentation in there.
+    |
+    | Available Settings: "single", "daily", "syslog", "errorlog"
+    |
+    */
+
+    'app_version' => '0.1',
 
     /*
     |--------------------------------------------------------------------------
@@ -109,6 +127,11 @@ return [
     */
 
     'providers' => [
+
+        /*
+         * Development Service Providers...
+         */
+        // Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Laravel Framework Service Providers...
@@ -136,14 +159,39 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Custom Services Providers...
+         */
+        Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
+        Vinkla\Pusher\PusherServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
+        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
+        Torann\GeoIP\GeoIPServiceProvider::class,
+        Alexpechkarev\GoogleGeocoder\GoogleGeocoderServiceProvider::class,
+
+        /*
+         * Personalization Services Providers...
+         */
+
+        /*
+         * Automation Services Providers...
+         */
+        Orangehill\Iseed\IseedServiceProvider::class,
+
+        /*
+         * My Services Providers...
+         */
 
     ],
 
@@ -160,39 +208,49 @@ return [
 
     'aliases' => [
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Bus'       => Illuminate\Support\Facades\Bus::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Input'     => Illuminate\Support\Facades\Input::class,
-        'Inspiring' => Illuminate\Foundation\Inspiring::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'App'           => Illuminate\Support\Facades\App::class,
+        'Artisan'       => Illuminate\Support\Facades\Artisan::class,
+        'Auth'          => Illuminate\Support\Facades\Auth::class,
+        'Blade'         => Illuminate\Support\Facades\Blade::class,
+        'Bus'           => Illuminate\Support\Facades\Bus::class,
+        'Cache'         => Illuminate\Support\Facades\Cache::class,
+        'Config'        => Illuminate\Support\Facades\Config::class,
+        'Cookie'        => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'         => Illuminate\Support\Facades\Crypt::class,
+        'DB'            => Illuminate\Support\Facades\DB::class,
+        'Eloquent'      => Illuminate\Database\Eloquent\Model::class,
+        'Event'         => Illuminate\Support\Facades\Event::class,
+        'File'          => Illuminate\Support\Facades\File::class,
+        'Hash'          => Illuminate\Support\Facades\Hash::class,
+        'Input'         => Illuminate\Support\Facades\Input::class,
+        'Inspiring'     => Illuminate\Foundation\Inspiring::class,
+        'Lang'          => Illuminate\Support\Facades\Lang::class,
+        'Log'           => Illuminate\Support\Facades\Log::class,
+        'Mail'          => Illuminate\Support\Facades\Mail::class,
+        'Password'      => Illuminate\Support\Facades\Password::class,
+        'Queue'         => Illuminate\Support\Facades\Queue::class,
+        'Redirect'      => Illuminate\Support\Facades\Redirect::class,
+        'Redis'         => Illuminate\Support\Facades\Redis::class,
+        'Request'       => Illuminate\Support\Facades\Request::class,
+        'Response'      => Illuminate\Support\Facades\Response::class,
+        'Route'         => Illuminate\Support\Facades\Route::class,
+        'Schema'        => Illuminate\Support\Facades\Schema::class,
+        'Session'       => Illuminate\Support\Facades\Session::class,
+        'Storage'       => Illuminate\Support\Facades\Storage::class,
+        'Str'           => Illuminate\Support\Str::class,
+        'URL'           => Illuminate\Support\Facades\URL::class,
+        'Validator'     => Illuminate\Support\Facades\Validator::class,
+        'View'          => Illuminate\Support\Facades\View::class,
+        'Form'          => Illuminate\Html\FormFacade::class,
+        'Html'          => Illuminate\Html\HtmlFacade::class,
+        'Entrust'       => Zizaco\Entrust\EntrustFacade::class,
+        'PusherManager' => Vinkla\Pusher\Facades\Pusher::class,
+        //'Image'         => Intervention\Image\Facades\Image::class,
+        'AWS'           => Aws\Laravel\AwsFacade::class,
+        'Calendar'      => MaddHatter\LaravelFullcalendar\Facades\Calendar::class,
+        'Socialite'     => Laravel\Socialite\Facades\Socialite::class,
+        'GeoIP'         => Torann\GeoIP\GeoIPFacade::class,
+        'Geocoder'      => Alexpechkarev\GoogleGeocoder\GoogleGeocoderServiceProvider::class,
 
     ],
 
